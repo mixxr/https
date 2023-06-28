2d0300d8143c86115a94dea95278a72d5ab62e5f
Michelangelo Serpico <mise@OSX-MISE-N-IT.homenet.telecomitalia.it>
2023-06-28 12:49:12 +0200

    re

--- app.js
+++ app.js
@@ -1,32 +1,97 @@
 // Import builtin NodeJS modules to instantiate the service
-const https = require("https");
-const fs = require("fs");
-
+//const https = require("https");
+//const fs = require("fs");
+const http = require("http");
 // Import the express module
 const express = require("express");
+const createError = require('http-errors');
 
 // Instantiate an Express application
 const app = express();
 
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/* GET home page. */
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(function(req, res, next) {
+  console.log("createError");
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 // Create a NodeJS HTTPS listener on port 4000 that points to the Express app
 // Use a callback function to tell when the server is created.
-https
-  .createServer(
-		// Provide the private and public key to the server by reading each
-		// file's content with the readFileSync() method.
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(443, () => {
-    console.log("serever is runing at port 443");
-  });
+var server = http.createServer(app);
 
-// Create an try point route for the Express app listening on port 4000.
-// This code tells the service to listed to any request coming to the / route.
-// Once the request is received, it will display a message "Hello from express server."
-app.get('/', (req,res)=>{
-    res.send("Hello from express server.")
-})
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  console.log("onError");
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  console.log("onListening");
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
